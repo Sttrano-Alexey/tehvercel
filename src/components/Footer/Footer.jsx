e7348916b090b07e907fa2cdf6270151a9349442
ssttrano <akimovalexey99@gmail.com>
2024-05-01 13:04:13 +0300

    footer links mod

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,22 +13,22 @@ export default function Footer(){
                 </div>
                 <nav className="footer__menu">
                     <ul className="footer__menu-list">
-                        <li className="footer__menu-item"><a href="delivery.html" className="footer__menu-link">Доставка</a></li>
-                        <li className="footer__menu-item"><a href="person.html" className="footer__menu-link">Юр. Лицам</a></li>
-                        <li className="footer__menu-item"><a href="payment.html" className="footer__menu-link">Оплата</a></li>
-                        <li className="footer__menu-item"><a href="safeguard.html" className="footer__menu-link">Гарантия</a></li>
-                        <li className="footer__menu-item"><a href="about.html" className="footer__menu-link">О нас</a></li>
-                        <li className="footer__menu-item"><a href="contacts.html" className="footer__menu-link">Контакты</a></li>
+                        <li className="footer__menu-item"><a href="delivery" className="footer__menu-link">Доставка</a></li>
+                        <li className="footer__menu-item"><a href="legal" className="footer__menu-link">Юр. Лицам</a></li>
+                        <li className="footer__menu-item"><a href="pay" className="footer__menu-link">Оплата</a></li>
+                        <li className="footer__menu-item"><a href="safeguard" className="footer__menu-link">Гарантия</a></li>
+                        <li className="footer__menu-item"><a href="about" className="footer__menu-link">О нас</a></li>
+                        <li className="footer__menu-item"><a href="contacts" className="footer__menu-link">Контакты</a></li>
                     </ul>
                 </nav>
                 <div className="footer__contact">
                     <div className="footer__item">
                         <img src={mail} alt="" className="footer__item-img" />
-                        <a href="" className="footer__item-link">1@techpo.ru</a>
+                        <a href="mailto:1@techpo.ru" className="footer__item-link">1@techpo.ru</a>
                     </div>
                     <div className="footer__item">
                         <img src={phone} alt="" className="footer__item-img" />
-                        <a href="" className="footer__item-link">8-910-00-00</a>
+                        <a href="tel:8-910-00-00" className="footer__item-link">8-910-00-00</a>
                     </div>
                 </div>
                 <div className="footer__adres">
