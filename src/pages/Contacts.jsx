64464497d481a37ea0e9103f75390dd9c1bfe010
ssttrano <akimovalexey99@gmail.com>
2024-05-01 12:59:23 +0300

    contact mod

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,6 +2,8 @@ import '../styles/descPage.css'
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer"
 import contacts from '../components/Images/img/contacts.jpg'
+import mail from '../components/Images/UI/mail.svg'
+import phone from '../components/Images/UI/phone.svg'
 
 export default function Contacts(){
     return(
@@ -12,11 +14,29 @@ export default function Contacts(){
                 <div className="description__column">
                     <h1 className="description__column-title">Контактная информация</h1>
                     <div className="description__column-text">
-                        <p className="description__column-p">Команда «ТехПо» успешно работает в сфере поставок IT оборудования <span>более 8 лет</span></p>
-                        <p className="description__column-p">На рынке с 2015 года</p>
-                        <p className="description__column-p">Приятная цена</p>
-                        <p className="description__column-p">Широкий ассортимент товаров</p>
-                        <p className="description__column-p">Удобная доставка</p>
+                        <div>
+                            <img src={phone} alt="" />
+                            <a href="tel:8-910-00-00" className="description__column-a">8-910-00-00</a>
+                        </div>
+                        <div className='mb'>
+                            <img src={mail} alt="" />
+                            <a href="mailto:1@techpo.ru" className="description__column-a">1@techpo.ru</a>
+                        </div>
+                        <p className="description__column-p"><strong>Реквизиты:</strong></p>
+                        <div className='desc-rec mb'>
+                            <span>ООО "АРМАДА"</span>
+                            <span>ИНН: 4028073447</span>
+                            <span>КПП: 402801001</span>
+                            <span>ОГРН: 1224000008957</span>
+                            <span>Расчетный счет:</span>
+                            <span>40702810822240010228</span>
+                            <span>Банк:</span>
+                            <span>КАЛУЖСКОЕ ОТДЕЛЕНИЕ N8608 ПАО СБЕРБАНК</span>
+                            <span>БИК: 042908612</span>
+                            <span>Корр. счет: 30101810100000000612</span>
+                        </div>
+                        <p className="description__column-p"><strong>Юридический адрес</strong></p>
+                        <p className="description__column-p adres">248010, Калужская обл, г Калуга, ул Высокая, д. 2а, кв. 114</p>
                     </div> 
                 </div>
                 <div className="description__column">
